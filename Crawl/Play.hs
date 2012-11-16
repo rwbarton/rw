a136f18ed1d2ba854bf06c0062791665260b41ee
Reid Barton <rwbarton@gmail.com>
2012-11-16 11:54:36 -0500

    Rests, reactively!

--- Crawl/Play.hs
+++ Crawl/Play.hs
@@ -9,15 +9,32 @@ import Control.Monad (forever)
 import Control.Concurrent.Chan.Split (InChan, OutChan, writeChan, readChan)
 import qualified Data.Aeson as A
 import qualified Data.HashMap.Strict as H
+import qualified Reactive.Banana as R
+import qualified Reactive.Banana.Frameworks as R
 
 play :: OutChan A.Object -> InChan A.Object -> IO ()
-play recvChan sendChan = forever $ do
-  msg <- readChan recvChan
-  writeChan sendChan $ H.fromList [
-    ("msg", "input"),
-    ("text", ".")
-    ]
-  writeChan sendChan $ H.fromList [
-    ("msg", "input"),
-    ("text", " ")
-    ]
+play recvChan sendChan = do
+  (recvHandler, recvFire) <- R.newAddHandler
+  let sendHandler = writeChan sendChan
+  network <- R.compile $ setupNetwork recvHandler sendHandler
+  R.actuate network
+  forever $ do
+    msg <- readChan recvChan
+    recvFire msg
+    
+setupNetwork :: R.Frameworks t => R.AddHandler A.Object -> (A.Object -> IO ()) -> R.Moment t ()
+setupNetwork recvHandler sendHandler = do
+  input <- R.fromAddHandler recvHandler
+  let output = bananaCrawl input
+  R.reactimate $ fmap sendHandler output
+
+bananaCrawl :: R.Event t A.Object -> R.Event t A.Object
+bananaCrawl input = fmap (const rest) input `R.union` fmap (const space) input
+  where rest = H.fromList [
+          ("msg", "input"),
+          ("text", ".")
+          ]
+        space = H.fromList [
+          ("msg", "input"),
+          ("text", " ")
+          ]
