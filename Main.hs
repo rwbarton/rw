5b0eced7d038b2ffa9e593037ab12e9fe4d85447
Reid Barton <rwbarton@gmail.com>
2012-11-17 13:27:08 -0500

    Really primitive logging

--- Main.hs
+++ Main.hs
@@ -1,18 +1,32 @@
 module Main where
 
 import Control.Applicative ((<$>))
+import Control.Concurrent (forkIO)
+import Control.Monad (forever)
 import System.Environment (getArgs)
 import System.Exit (exitWith, ExitCode(ExitFailure))
-import System.IO (hPutStrLn, stderr)
+import System.IO (hPutStrLn, hPrint, stderr,
+                  openFile, IOMode(WriteMode))
+
+import Control.Concurrent.Chan.Split (OutChan, dupChan, readChan)
 
 import Crawl.AccountInfo
 import Crawl.Connect
 import Crawl.Login
 import Crawl.Play
 
+logReceived :: Show a => OutChan a -> IO ()
+logReceived recvChan = do
+  recvLogged <- dupChan recvChan
+  forkIO $ do
+    logFile <- openFile "rw.log" WriteMode
+    forever $ readChan recvLogged >>= hPrint logFile
+  return ()
+
 connectAndPlay :: AccountInfo -> IO ()
 connectAndPlay config = do
   (sendChan, recvChan) <- connectToWebtiles config
+  logReceived recvChan
   login sendChan config
   sendRCFile sendChan config
   startGame sendChan config
