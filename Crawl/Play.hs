beb6be3f41b2a9ea4f9504943f6748ad117e7c87
Reid Barton <rwbarton@gmail.com>
2012-11-17 01:01:08 -0500

    Demultiplex "multi" messages

--- Crawl/Play.hs
+++ Crawl/Play.hs
@@ -7,10 +7,13 @@ module Crawl.Play (
 import Control.Monad (forever)
 
 import Control.Concurrent.Chan.Split (InChan, OutChan, writeChan, readChan)
+import Control.Lens ((^.), (^..), at, traverse)
 import qualified Data.Aeson as A
 import qualified Data.HashMap.Strict as H
 import qualified Reactive.Banana as R
 import qualified Reactive.Banana.Frameworks as R
+
+import Crawl.AesonLens
 
 play :: OutChan A.Object -> InChan A.Object -> IO ()
 play recvChan sendChan = do
@@ -25,8 +28,15 @@ play recvChan sendChan = do
 setupNetwork :: R.Frameworks t => R.AddHandler A.Object -> (A.Object -> IO ()) -> R.Moment t ()
 setupNetwork recvHandler sendHandler = do
   input <- R.fromAddHandler recvHandler
-  let output = bananaCrawl input
+  let demultiplexed = R.spill $ fmap demultiplex input
+      output = bananaCrawl demultiplexed
   R.reactimate $ fmap sendHandler output
+
+demultiplex :: A.Object -> [A.Object]
+demultiplex msg
+  | Just "multi" == msg^.at "msg" =
+    concatMap demultiplex (msg^..at "msgs".traverse.asArray.traverse.asObject)
+  | otherwise = [msg]
 
 bananaCrawl :: R.Event t A.Object -> R.Event t A.Object
 bananaCrawl input = fmap (const rest) input `R.union` fmap (const space) input
