737cea7adfe6e5714f2bf506297314aa76d37bdd
Reid Barton <rwbarton@gmail.com>
2013-05-13 10:37:40 -0400

    Hack to work around invalid json

--- Crawl/Connect.hs
+++ Crawl/Connect.hs
@@ -6,6 +6,7 @@ import Control.Applicative ((<$>))
 import Control.Concurrent (forkIO)
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
+import Data.Char (ord)
 import Data.Maybe (fromMaybe)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
@@ -48,9 +49,17 @@ connectToWebtiles config = do
     forever $ do
       Binary compressed <- receiveDataMessage
       liftIO $ do
-        decompressed <- decompress inflator compressed
+        decompressed <- fmap jsonHack $ decompress inflator compressed
+        -- BL.putStrLn decompressed
         writeChan sendIncoming $
           fromMaybe (error "JSON decoding error") (A.decode decompressed)
   return (sendOutgoing, recvIncoming)
   where asHybi10 :: WebSockets Hybi10 a -> WebSockets Hybi10 a
         asHybi10 = id
+
+jsonHack :: BL.ByteString -> BL.ByteString
+jsonHack msg
+  | quoteMsgs `BL.isPrefixOf` msg = doubleQuoteMsgs `BL.append` BL.drop (BL.length quoteMsgs) msg
+  | otherwise = msg
+    where quoteMsgs       = BL.pack $ map (fromIntegral . ord) "{'msgs'"
+          doubleQuoteMsgs = BL.pack $ map (fromIntegral . ord) "{\"msgs\""
