cba700c1b78a9f388b5399f6855198a49dedd086
Reid Barton <rwbarton@gmail.com>
2013-10-19 15:25:15 -0400

    Track monster types

--- Crawl/LevelInfo.hs
+++ Crawl/LevelInfo.hs
@@ -2,11 +2,13 @@
 
 module Crawl.LevelInfo where
 
+import Control.Applicative (liftA2)
 import Control.Monad (mplus, when)
 import Control.Monad.Trans.State (execState, put)
 import Data.Bits (testBit)
+import Data.Maybe (fromMaybe)
 import qualified Data.Foldable as F
-import Control.Lens ((^?), (^..), (.=), (%~), _1, traverse, at, enum, contains, to)
+import Control.Lens ((^?), (^..), (.=), (%~), _1, traverse, at, enum, contains, to, use)
 import Control.Lens.TH (makeLenses)
 import Numeric.Lens (integral)
 import Control.Lens.Aeson (_Bool, _Array, _Integer, key)
@@ -20,20 +22,31 @@ import Crawl.Bindings
 
 type MapCell = Feature
 
-data Coord = Coord !Int !Int deriving (Eq, Ord)
+data Coord = Coord !Int !Int deriving (Eq, Ord, Show)
 instance H.Hashable Coord where
   hash (Coord x y) = x + 100 * y
 
+-- Want these to be strict, but how to catch uninitialized fields then?
+data Monster = Monster {
+  _monsterType :: Int
+  } deriving Show
+
+makeLenses ''Monster
+
+uninitializedMonster :: Monster
+uninitializedMonster = Monster (error "used uninitializedMonster monsterType!")
+
 data LevelInfo = LevelInfo {
   _levelMap :: !(H.HashMap Coord MapCell),
-  _levelMonsters :: !(HS.HashSet Coord),
+  _levelMonsters :: !(H.HashMap Coord Monster),
+  _levelMonsterTable :: !(H.HashMap Int Monster),
   _levelLoot :: !(HS.HashSet Coord)
   }
 
 makeLenses ''LevelInfo
 
 emptyLevel :: LevelInfo
-emptyLevel = LevelInfo H.empty HS.empty HS.empty
+emptyLevel = LevelInfo H.empty H.empty H.empty HS.empty
 
 levelInfo :: R.Event t A.Value -> R.Behavior t LevelInfo
 levelInfo input = R.accumB emptyLevel $ fmap (execState . updateLevel) input
@@ -41,8 +54,23 @@ levelInfo input = R.accumB emptyLevel $ fmap (execState . updateLevel) input
           where cellMsgs = withCoordinates $ msg ^.. key "cells"._Array.traverse
                 updateCell (coord, cellMsg) = do
                   F.mapM_ (levelMap.at coord .=) (cellMsg ^? key "f"._Integer.integral.enum.to Just)
-                  F.mapM_ (levelMonsters.contains coord .=) (cellMsg ^? key "mon".to (/= A.Null))
+                  F.mapM_ updateMonster (cellMsg ^? key "mon")
                   F.mapM_ (levelLoot.contains coord .=) (cellMsg ^? key "t".key "bg"._Integer.to (`testBit` 20))
+                  where updateMonster monsterData =
+                          -- This crazy logic is intended to duplicate merge_monster
+                          -- from webserver/game_data/static/map_knowledge.js
+                          -- todo: garbage collect levelMonsterTable
+                          case monsterData of
+                            A.Null -> levelMonsters.at coord .= Nothing
+                            _ -> do
+                              let monsterID = monsterData ^? key "id"._Integer.integral
+                              oldMonster <- liftA2 (\x y -> fromMaybe uninitializedMonster $ mplus x y)
+                                            (maybe (return Nothing) (\mid -> use (levelMonsterTable.at mid)) monsterID)
+                                            (use (levelMonsters.at coord))
+                              let newMonster = (`execState` oldMonster) $ do
+                                    F.mapM_ (monsterType .=) (monsterData ^? key "type"._Integer.integral)
+                              levelMonsters.at coord .= Just newMonster
+                              F.forM_ monsterID $ \mid -> levelMonsterTable.at mid .= Just newMonster
 
 withCoordinates :: [A.Value] -> [(Coord, A.Value)]
 withCoordinates vs = map (_1 %~ (\(Just x, Just y) -> Coord x y)) $ scanl1 f $ map extractCoordinates vs
