769d8d24d17e386a351b17409b92a2ebff496321
Reid Barton <rwbarton@gmail.com>
2015-11-13 16:05:12 -0500

    Don't try to ctrl-attack while confused

--- Crawl/BadForms.hs
+++ Crawl/BadForms.hs
@@ -12,6 +12,8 @@ import Crawl.Status
 data Form = Normal | Tree | Fungus
 
 filterLegalInForm :: Player -> Maybe Move -> Maybe Move
+filterLegalInForm p (Just (Attack dx dy))
+  | isConfused p = filterLegalInForm p (Just (Go dx dy))
 filterLegalInForm p (Just m)
   | legalInForm p m = Just m
 filterLegalInForm _ _ = Nothing
